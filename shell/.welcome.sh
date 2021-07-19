bc4856c5f0afec160ea12ead6a32447f3e792780
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2021-07-19 16:27:38 -0400

    Maint: update .welcome.sh

--- shell/.welcome.sh
+++ shell/.welcome.sh
@@ -1,3 +1,2 @@
 echo "Terminal Docker tools aliases:"
-echo " * alpine: launch an interactive alpine 3.12 container"
-echo " * dive: inspect the layers of a Docker image"
+echo " * alpine: launch an interactive alpine 3.14 container"
